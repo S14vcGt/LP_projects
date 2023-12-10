40927add1911a05d9be7e19f8ee736043ca740d1
Sebastian Mata <sebasmata41@gmail.com>
2023-12-10 18:36:08 -0400

    "terminado"

--- segundo_projecto/ejemplos.rkt
+++ segundo_projecto/ejemplos.rkt
@@ -19,23 +19,3 @@
   (if (null? x)
       ( error "no se permiten listas vacias")
       ( list-ref x 0)))
-
-(let*([vistos 1]
-                        [mayor (car fila)])
-                    (for ([i (in-range tam)])
-                      (if (> (list-ref fila i) mayor )
-                          ((set! vistos (add1 vistos)) (set! mayor (list-ref fila i))) #t))
-                    (if (not(= vistos (list-ref (list-ref vistas 2) fil)))
-                        #f
-                        #t))
-
-(define (este-sur tam fil-col vistas vertex index)
-  (let*([vistos 1]
-        [mayor (car fil-col)])
-          (for ([i (in-inclusive-range (sub1 tam) 0 -1)])
-                (if (> (list-ref fil-col i) mayor )
-                     ((set! vistos (add1 vistos)) (set! mayor (list-ref fil-col i)))#t))
-          (if (not(= vistos (list-ref (list-ref vistas index) vertex)))
-                        #f
-                        #t)))
-  
--- segundo_projecto/segundo_proyecto.rkt
+++ segundo_projecto/segundo_proyecto.rkt
@@ -3,7 +3,7 @@
 
 (define (asignar-valido tam vistas); me entrega una matriz tam x tam con los numeros posibles en cada casilla segun las vistas
   (for*/list ([i(in-range tam)]); una lista de vectores
-    (for*/vector ([j(in-range tam)]); , to speed up the algorithm i put list with the most likely numbers so it just can pick one of those 
+    (for*/vector ([j(in-range tam)])
       (cond; dependiendo de la vista que corresponda con la casilla actual, se le asignan los numeros probables con la formula (tam - vistas[i][j])+1
         [ (= i 0) (if (= (list-ref(car vistas) j) 1)
                       (list tam)
@@ -19,66 +19,53 @@
                                (for*/list ([k(in-range 1 (+ 2 (- tam (list-ref(list-ref vistas 3)i))))])k))]
         [else (for*/list ([k(in-range 1 (add1 tam))])k)]))))
 
-(define (verificador tam fil-col vistas vertex index); verifica las vistas en los 4 puntos cardinales, verify the views in the 4 cardinal points
-  (let*([vistos 1]
-        [mayor (car fil-col)])
-          (for ([i (in-range tam)])
-                (when (> (list-ref fil-col i) mayor ); aqui retorna void cuando no se cumple
-                     ((set! vistos (add1 vistos))
-                      (set! mayor (list-ref fil-col i))))
-          (if (not(= vistos (list-ref (list-ref vistas index) vertex))); vertex me indica la fila o columna en la que estamos ahora, e index la lista que contiene las vistas del punto cardinal que queremos verificar
-                        #f
-                        #t))))
-
-(define (elegible grilla fil col tam vistas valor); me dice si el valor que se quiere asignar a la casilla no esta repetido, it tells if the value is not repeated
+(define (verificador tam fil-col vistas vertex index); verifica las vistas en los 4 puntos cardinales
+  (let loop([vistos 0] [mayor 0] [rr fil-col])
+    (cond
+      [(null? rr) (= vistos (list-ref (list-ref vistas index) vertex))]; vertex me indica la fila o columna en la que estamos ahora,
+      [(list? (car rr)) #t]
+      [(> (car rr) mayor) (loop (add1 vistos) (car rr) (cdr rr) )]; e index la lista que contiene las vistas del punto cardinal que queremos verificar
+      [else (loop vistos mayor (cdr rr))])))
+  
+(define (elegible? grilla fil col tam valor); me dice si el valor que se quiere asignar a la casilla no esta repetido, it tells if the value is not repeated
    (let* ([fila (vector->list (list-ref grilla fil))]
          [columna (map (lambda (x) (vector-ref (list-ref grilla x) col)) (range tam))]
          [in-fila? (member valor fila)]
          [in-col? (member valor columna)])
-    (display fila) (newline)
-     (display columna)(newline)
-     (display " no idea")(newline)
-     (display in-fila?) (newline)
-     (display in-col?) (newline)
-     (if (or in-fila? in-col?); si el valor esta en la fila o en la columna actual, false 
-        #f
-        #t)))
-
+     (not (or in-fila? in-col?)))); si el valor esta en la fila o en la columna actual, false 
+        
 (define (vigilante grilla fil col tam vistas); me dice si los valores asignados despues de terminar una fila cumplen con las vistas
-   (let* ([fila (vector->list (list-ref grilla fil))]
-         [columna (map (lambda (x) (vector-ref (list-ref grilla x) col)) (range tam))])
-    (let([oeste (verificador tam fila vistas fil 2)]
-                 [norte (verificador tam columna vistas col 0)]
-                 [este (verificador tam (reverse fila) vistas fil 3)]; revierto la fila o columna para evaluar en la otra direccion
-                 [sur (verificador tam (reverse columna) vistas col 1)])
-              (if (and (and oeste norte) (and este sur))
-                  #t
-                  #f)))); si todas las vistas corresponde, true sino false
+  (if (or (= fil (sub1 tam)) (= col (sub1 tam)))
+      (let* ([fila (vector->list (list-ref grilla fil))]
+            [columna (map (lambda (x) (vector-ref (list-ref grilla x) col)) (range tam))]
+            [oeste (verificador tam fila vistas fil 2)]
+            [norte (verificador tam columna vistas col 0)]
+            [este (verificador tam (reverse fila) vistas fil 3)]
+            [sur (verificador tam (reverse columna) vistas col 1)])
+        (and (and oeste norte) (and este sur)))#t))
               
 (define (resolve grilla tam fil col vistas)
   (cond
-    [(= fil tam) (display(for*/list ([i (in-range tam)]) ;si ya llega al final, se imprime la grilla
-                          (vector->list (list-ref grilla i))))]
-    [(= col (sub1 tam)) (resolve grilla tam (add1 fil) 0 vistas)]; si es la ultima columna, se pasa a la siguiente fila
-    [else ((for-each (lambda (r) (display r) (newline)) grilla) (let*([posibles (vector-ref (list-ref grilla fil)col)]; la variable con la lista de los posibles valores
-                 [result #f]; el resultado
-                 [valor 0]);el valor a probar
-             (for* ([ i (in-range (length posibles))])
-               (if ( equal? result #f)
-                   (begin (set! valor (list-ref posibles i)); here is where the debugger find the error
-                    (let ([ aver (elegible grilla fil col tam vistas valor)]); just for debugging elegible
-                    (if (equal? aver #t)
-                      ( begin (vector-set! (list-ref grilla fil) col valor); usando cond
-                      (if (= (- tam 1) col)
-                        (if (not (vigilante grilla fil col tam vistas))
-                          (begin (vector-set! (list-ref grilla fil) col posibles)
-                          #f) #f) #f)
-                        (begin (set! result (resolve grilla tam fil (add1 col) vistas))))#f)))
-                    result))
-             (vector-set! (list-ref grilla fil) col posibles) #f))]))
-
+    [(= fil tam) grilla]
+    [(= col tam) (resolve grilla tam (add1 fil) 0 vistas)]; si es la ultima columna, se pasa a la siguiente fila
+    [else (let ([posibles (filter (lambda (n) (elegible? grilla fil col tam n)) (vector-ref (list-ref grilla fil)col))]
+                [rollback (vector-ref (list-ref grilla fil)col)]); lista de valores posibles
+            (if (null? posibles)
+                #f
+                (let loop ([valores posibles])
+                  (if (null? valores)
+                      ((lambda (n) (vector-set! (vector-ref (vector-copy (list->vector n)) fil) col rollback) #f) grilla)
+                      (let ([grilla-copy ((lambda (n) (vector-set! (vector-ref (vector-copy (list->vector n)) fil) col (car valores)) n) grilla)])
+                        (if(vigilante grilla-copy fil col tam vistas)
+                           (let ([result (resolve grilla-copy tam fil (add1 col) vistas)])
+                             (if result
+                                 result
+                                 (loop(cdr valores))))
+                           (loop(cdr valores))))))))]))
+                         
 (define (vistas lista)
-  (let* ([grilla (asignar-valido (length (car lista )) lista)])
-    (resolve grilla (length lista) 0 0 lista)))
-; in (vistas'((3 1 2 2) (2 3 1 2) (2 2 1 3) (3 1 3 2)))
-; supossed out ((2 4,3,1), (3,1,2,4), (4,3,1,2), (1,2,4,3))
+  (let* ([ tam (length (car lista ))]
+         [grilla (asignar-valido tam lista)])
+    (display (resolve grilla tam 0 0 lista))))
+; in (vistas'((2 1 3 3 4) (3 3 2 2 1) (2 1 3 2 4) (3 5 3 2 1)))
+; supossed out ((4 5 1 3 2), (5 4 3 2 1), (1 2 5 4 3), (3 1 2 5 4) (2 3 4 1 5))
