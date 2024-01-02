67151641ed61e52fd32550ff210e9f4b1b843082
Sebastian Mata <sebasmata41@gmail.com>
2024-01-02 14:52:27 -0400

    "ya"

--- segundo_projecto/ejemplos.rkt
+++ segundo_projecto/ejemplos.rkt
@@ -9,7 +9,7 @@
       y))
     
 
-(define(primo x)
+(define(primo? x)
   (equal? (divisores x) (list 1 x)))
 
 (define (largo x)
@@ -18,4 +18,30 @@
 (define (primero x)
   (if (null? x)
       ( error "no se permiten listas vacias")
-      ( list-ref x 0)))
+      ( car x)))
+
+(define (primo-hasta x)
+  (for/list ([y (in-inclusive-range 1 x)]
+             #:when (primo? y))
+      y))
+
+(define(pares-hasta x)
+  (for/list ([y (in-range 1 (+ 1 x))]
+             #:when (divisible? y 2))
+      y))
+
+(define (sumar z)
+  (if (null? z)
+      0
+      (+ (car z) (sumar (cdr z)))))
+
+(define (sumar-rango z)
+  (let ([ y (for/list ([ x (in-inclusive-range 1 z)]) x)])
+    (sumar y)))
+
+(define (sumar-rango-primo z)
+  (let ([ y (primo-hasta z)]);([ y (filter (lambda (n) (primo? n)) (for/list ([ x (in-inclusive-range 1 z)])x))])
+    (sumar y)))
+
+(define (sublist? x y)
+  (andmap (lambda (n) (if (member n y) #t #f)) x)) 
