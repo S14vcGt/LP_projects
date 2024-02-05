0b4ce457e423613f7ad4fe2f7d5f05cbe6fbf891
Sebastian Mata <sebasmata41@gmail.com>
2024-02-05 00:09:23 -0400

    avances proyecto 3

--- 3er_proyecto/3_son_multitud.pl
+++ 3er_proyecto/3_son_multitud.pl
@@ -7,43 +7,69 @@ matriz([[0,_,_,_,_,_], [_,_,1,0,_,_], [_,_,1,1,_,_], [_,_,_,0,1,_],
 es_anonima(Var):-
 var(Var),
 \+ \+ Var = _,!.
+ 
 
 len([],0):-!.
-len([X|Y],S):-len(Y,T),!, S is T + 1.
+len([_|Y],S):-len(Y,T),!, S is T + 1.
 
 lista([]):-!.
-lista([X|Y]):-lista(Y).
+lista([_|Y]):-lista(Y).
 
-da_n_pri(L,0,[]):-!.
-da_n_pri([],N,[]):-!.
-da_n_pri([X|M],1,[1]):- es_anonima(X).
-da_n_pri([X|M],1,[X]):-!.
+revertir_lista([], []).
+revertir_lista([X|L], S) :- revertir_lista(L, R), append(R, [X], S).
+
+transponer(_,0,[]):-!.
+transponer(X,N,M):-
+    maplist(nth1(N),X,A),
+    revertir_lista(A,S),
+    N1 is N-1,
+    transponer(X,N1,W),
+    append(W,[S],M).
+%
+
+rotar([],[]).
+rotar(L,M):- len(L,N), transponer(L,N,M).
+
+da_n_pri(_,0,[]):-!.
+da_n_pri([],_,[]):-!.
+da_n_pri([X|_],1,[1]):- es_anonima(X).
+da_n_pri([X|_],1,[X]):-!.
 da_n_pri([X|M],N,S):-
 N1 is N -1,
 es_anonima(X),
 da_n_pri(M,N1,T),
-concatenar([1],T,S).
+append(T,[1],S).
 da_n_pri([X|M],N,S):-
 N1 is N -1,
 da_n_pri(M,N1,T),
-concatenar([X],T,S).
-
-revertir_lista([], []).
-revertir_lista([X|Resto], Revertida) :-
-    revertir_lista(Resto, RestoRevertido),
-    append(RestoRevertido, [X], Revertida).
-%
+append(T,[X],S).
 
 da_n_ultim(L,N,S):-
 revertir_lista(L,T),
 da_n_pri(T,N,R),
 revertir_lista(R,S).
 
-recorrer(_,0,[]):-!.
-recorrer([X|L],N,R):-
-    
+comparar(_,[],_):-!.
+comparar(N,[M|L],X):- X \== M, comparar(N,L,X).
+comparar(0,[M|L],X):- X == M , comparar(1,L,X).
+unique(M):-maplist(comparar(0,M),M).
 
-3_son_multitud([],[]):-!.
-3_son_multitud(L,R):-
-len(L,N),
-recorrer(L,N,R).
+
+replace(Z,Var):-var(Var), !, Z in 0..1.
+replace(X,X).
+rr(L,M):-maplist(replace,L,M).
+
+cross(2,L,R):-rotar(L,S),rotar(S,R).
+cross(N,L,R):-
+rr(L,M),
+unique(M),
+balanced(M),
+dos_y_ya(M),
+rotar(M,S),
+N1 is N+1,
+cross(N1,S,R).
+
+
+tres_son_multitud([],[]):-!.
+tres_son_multitud(L,R):-
+maplist(cross(0,L),L,R).
